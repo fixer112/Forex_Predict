f8fac4986202e0ddc047f500e566a424046ec9c8
Abubakar Lawal <abula3003@gmail.com>
2019-03-26 11:09:29 +0100

    Cache data everyday
    Now data is feched  perday to stop wastage of request and bandwith

--- forex_predict.php
+++ forex_predict.php
@@ -19,6 +19,17 @@ function get($url){
     return $response;
 }
 
+function store($file, $content){
+    file_put_contents($file, json_encode($content));
+}
+
+function fetch($file){
+    if (file_exists($file)) {
+        return file_get_contents($file);
+    }
+    return false;
+}
+
 $error = [];
 $curr = ['USD','GBP','JPY','EUR', 'AUD', 'CAD'];
 
@@ -31,10 +42,11 @@ if (check()) {
     $symbol = $_GET['symbol'];
     $currency_predict = $_GET['predict'];
     $days = $_GET['days'];
+    $filename = "data.json";
 
-    /*if ($base == $symbol) {
+    if ($base == $symbol) {
         array_push($error, 'Base and symbol cant be the same');
-    }*/
+    }
 
     if (!is_numeric($currency_predict) || $currency_predict < 0 ) {
         array_push($error, 'Predict Rate should be greater than zero and should be numeric');
@@ -48,22 +60,41 @@ if (check()) {
         array_push($error, 'Data days must be odd number');
     }
 
+    if ($days < 3) {
+        array_push($error, 'Data days must not be lesser than 3');
+    }
+
+
      /*if ($days > 200) {
         array_push($error, 'Data days must not be greater than 200');
     }*/
 
     if(count($error) == 0){
-        $api = file_get_contents("api.txt");
+        $data = json_decode(fetch($filename),true);
+        $data_source = "Cached Data saved in data.json";
+        if (!$data || (isset($data['date']) && $data['date'] < Carbon::now()->subDays(1)) || !isset($data['history'])) {
+        $api = fetch("api.txt");
         $domain = 'https://www.worldtradingdata.com/api/v1/forex_history';
         $url = $domain.'?api_token='.$api.'&base='.$base.'&convert_to='.$symbol;
         $result = get($url);
         $result = json_decode($result,true);
-        if (isset($result['Message'])) {
-            array_push($error, $result['Message']);
+        $result['date'] = Carbon::now()->format('Y-m-d');
+        store($filename, $result);
+
+        $data = json_decode(fetch($filename),true);
+        $data_source = $domain;
+         }
+
+        if($days > count($data['history'])){
+            array_push($error, 'Data days must not be greater than '.count($data['history']));
+        }
+
+        if (isset($data['Message'])) {
+            array_push($error, $data['Message']);
         }
 
         if(count($error) == 0){
-        $history = $result['history'];
+        $history = $data['history'];
         $from = Carbon::now()->startOfDay();
         $start = Carbon::now()->startOfDay();
 
@@ -129,10 +160,10 @@ if (check()) {
                 <div class="form-group">
                     <label>Base</label>
                     <select name="base" class="form-control" required>
-                        <option value=''>Choose</option>
+                        <option value='' <?php !isset($base)? "selected" : ""?> >Choose</option>
                         <?php
                         foreach ($curr as $cur) {
-                            $check = isset($base) && $base == $cur ? "selected" : $cur == "GBP" ? "selected" : "";
+                            $check = isset($base) && $base == $cur ? "selected"  : "";
                             echo "<option ".$check." value=".$cur.">".$cur."</option>";
                         }
                         ?>
@@ -143,10 +174,10 @@ if (check()) {
                 <div class="form-group">
                     <label>Symbol</label>
                     <select name="symbol" class="form-control" required>
-                        <option value='' >Choose</option>
+                        <option value='' <?php !isset($base)? "selected" : ""?> >Choose</option>
                         <?php
                         foreach ($curr as $cur) {
-                            $check = isset($symbol) && $symbol == $cur ? "selected" : $cur == "USD" ? "selected" : "";
+                            $check = isset($symbol) && $symbol == $cur ? "selected" : "";
                             echo "<option ".$check." value=".$cur.">".$cur."</option>";
                         }
                         ?>
@@ -182,11 +213,11 @@ if (check()) {
             echo '<div class="response bg-success">';
             $knn->train($currencys, $bs);
             $nb->train($currencys, $bs);
-            echo 'Forex data feched from '.$domain.' for <strong>'.$base.'/'.$symbol.'</strong> pair from '.$start->format('Y-m-d').' to '.$from->format('Y-m-d').' ('.$days.' Days) <br>';
+            echo 'Forex data feched from '.$data_source.' for <strong>'.$base.'/'.$symbol.'</strong> pair from '.$start->format('Y-m-d').' to '.$from->format('Y-m-d').' ('.$days.' Days) <br>';
             echo 'Last close rate is '.$currencys[count($currencys)-1][1].'<br>';
-            echo '<strong>'.$knn->predict([$currencys[count($currencys)-1][1], $currency_predict]).' '.$result['symbol'].' @ '.$currency_predict.' for KNearestNeighbors. </strong><br>';
+            echo '<strong>'.$knn->predict([$currencys[count($currencys)-1][1], $currency_predict]).' '.$data['symbol'].' @ '.$currency_predict.' for KNearestNeighbors. </strong><br>';
 
-            echo '<strong>'.$nb->predict([$currencys[count($currencys)-1][1], $currency_predict]).' '.$result['symbol'].' @ '.$currency_predict.' for NaiveBayes. </strong><br>';
+            echo '<strong>'.$nb->predict([$currencys[count($currencys)-1][1], $currency_predict]).' '.$data['symbol'].' @ '.$currency_predict.' for NaiveBayes. </strong><br>';
             echo "</div>";
         }
         ?>
